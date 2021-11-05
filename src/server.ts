ceca75e2264847028961c482441a04ac6c974297
Blake Abel <blakecabel@gmail.com>
2021-11-05 10:21:21 -0400

    Added env file + code that reads it

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,9 @@
+import dotenv = require('dotenv');
 import app from "./app";
 
-const port = 3000;
+dotenv.config();
+
+const port = process.env.SERVER_PORT;
 
 const server = app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
