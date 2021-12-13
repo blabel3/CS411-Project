18c7f785042e6b64501ccda8b44fc183cfe0b7dc
RichardChen123 <richard.chen.517@gmail.com>
2021-12-12 21:39:34 -0500

    should be giving us an image now

--- src/controllers/unsplash.ts
+++ src/controllers/unsplash.ts
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import {playlistEndpoint} from "./spotify"
 
 const unsplash = axios.create({
     baseURL: 'https://api.unsplash.com',
@@ -8,13 +9,37 @@ const unsplash = axios.create({
     }
 });
 
+const searchTermsMap = {
+    danceability: {high: "dance", threshold: 0.5},
+    energy: {low: "calm", high: ["energetic", "energy", "noisy", "loud", "fast"], threshold: 0.5},
+    loudness: {low: ["quiet", "soft"], high: ["loud"], threshold: -15},
+    speechiness: {high: ["rap", "song"], low: ["music"], threshold: 0.5},
+    acousticness: {high: ["acoustic"], low: ["electronic"], threshold: 0.5},
+    instrumentalness: {high: ["instrument", "instrumental"], low: ["vocal", "voice", "rap"], threshold: 0.5},
+    liveness: {high: ["audience", "concert"], low: ["record", "recording"], threshold: 0.5},
+    valence: {high: ["happy", "cheerful", "euphoric"], low: ["sad", "depressed", "angry"], threshold: 0.5},
+    tempo: {high: "fast", low: ["slow", "R&B"], threshold: 120}
+};
 
+const audioFeaturesToSearchTerms = (playlistAudioFeatures) =>  {
+    const searchTermsList = new Set();
+    for (const feature in playlistAudioFeatures) {
+        if (playlistAudioFeatures[feature] > searchTermsMap[feature].threshold) {
+            searchTermsList.add(searchTermsMap[feature].high);
+        }
+        else {
+            searchTermsList.add(searchTermsMap[feature].low);
+        }
+    }
+    return searchTermsList;
+}
 
 
 
 export const getRandomPhoto = (req, res) => {
-
-    let query = '';
+    const playlistAudioFeatures = playlistEndpoint(req, res);
+    const searchTerms = audioFeaturesToSearchTerms(playlistAudioFeatures);
+    let query = [...searchTerms].join(',');
     if (req.query.search) {
         query = `?query=${req.query.search}`;
     }
